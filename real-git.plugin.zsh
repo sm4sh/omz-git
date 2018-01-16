eaee5eba1288c208cf50326c21d97d8f6840b51f
Bastian Böhne <bastian.boehne@hitmeister.de>
2018-01-16 16:29:17 +0100

    finnally fixed

--- real-git.plugin.zsh
+++ real-git.plugin.zsh
@@ -1,16 +1,18 @@
 function git_prompt_info() {
+  local ref
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-
-    branch=$ref
-    if ! [[ $( echo "$branch" | egrep -i "^dev-[0-9]+") ]]; then
-      echo "$branch"
-      exit
+    ref=${ref#refs/heads/}
+    if [ "$(echo "$ref" | egrep -i "^dev-[0-9]+")" = "" ];
+    then
+      echo "$ref"
+      echo "regex failed"
+      return 0
     fi
-    ref=$( echo "$branch" | egrep -io "dev-[0-9]+")
-
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}${parse_git_dirty}$ZSH_THEME_GIT_PROMPT_SUFF    IX"
+    ref=$( echo "$ref" | egrep -io "dev-[0-9]+")
+    echo "ende angelangt"
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
 
