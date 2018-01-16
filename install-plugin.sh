3e4f9f9a20646ee1371aa79535da3c3bca3c4b06
Bastian Böhne <bastian.boehne@hitmeister.de>
2018-01-16 14:17:58 +0100

    fixed clone adress

--- install-plugin.sh
+++ install-plugin.sh
@@ -2,7 +2,7 @@ pluginPath=${ZSH}/custom/plugins/real-git
 
 rm -rf ${pluginPath}
 
-git clone https://github.com/tnwinc/omz-git.git ${pluginPath}
+git clone https://github.com/sm4sh/real-git.git ${pluginPath}
 
 echo
 echo "Open your .zshrc file and add real-git to your plugins."
