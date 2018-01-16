29ca4e2c02a8b7e99f099369d43c5ed134c910d2
Bastian Böhne <bastian.boehne@hitmeister.de>
2018-01-16 15:17:46 +0100

    should work now

--- real-git.plugin.zsh
+++ real-git.plugin.zsh
@@ -1,18 +1,16 @@
 function git_prompt_info() {
-
-  local ref
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
 
-    branch=$(parse_git_dirty)
+    branch=$ref
     if ! [[ $( echo "$branch" | egrep -i "^dev-[0-9]+") ]]; then
       echo "$branch"
       exit
     fi
-    branchshort=$( echo "$branch" | egrep -io "dev-[0-9]+")
+    ref=$( echo "$branch" | egrep -io "dev-[0-9]+")
 
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}${branchshort}$ZSH_THEME_GIT_PROMPT_SUFF    IX"
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}${parse_git_dirty}$ZSH_THEME_GIT_PROMPT_SUFF    IX"
   fi
 }
 
