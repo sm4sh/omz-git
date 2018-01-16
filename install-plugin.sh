f1884142edee2012f41668457b68fd736b280d32
Bastian Böhne <bastian.boehne@hitmeister.de>
2018-01-16 13:47:36 +0100

    deleted unused files and corrected install script

--- install-plugin.sh
+++ install-plugin.sh
@@ -1,20 +1,11 @@
-pluginPath=${ZSH}/custom/plugins/omz-git
+pluginPath=${ZSH}/custom/plugins/real-git
 
 rm -rf ${pluginPath}
 
 git clone https://github.com/tnwinc/omz-git.git ${pluginPath}
 
 echo
-echo "Open your .zshrc file and add omz-git to your plugins."
-echo "You should end up with something like plugins=(git omz-git)."
+echo "Open your .zshrc file and add real-git to your plugins."
+echo "You should end up with something like plugins=(git real-git)."
 echo "Then, either restart your terminal or run source ~/.zshrc"
 echo
-
-read -p "Would you like to set up defaults for The Network? " -n 1 -r
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    echo "Run ${pluginPath}/tnwinc-setup.sh if you decide you want The Network's settings later."
-    exit 1
-fi
-
-${pluginPath}/tnwinc-setup.sh
