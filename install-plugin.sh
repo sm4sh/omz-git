5a2f990ec994a385a6631fbba7818420f7b81e8c
Bastian Böhne <bastian.boehne@hitmeister.de>
2018-01-16 17:12:51 +0100

    renamed install script

--- install-plugin.sh
+++
@@ -1,9 +0,0 @@
-customPath=${ZSH_CUSTOM}
-
-rm -rf ${customPath}/real-git.zsh
-
-curl -o ${customPath}/real-git.zsh https://raw.githubusercontent.com/sm4sh/real-git/master/real-git.zsh
-
-echo
-echo "Restart your terminal to enable"
-echo
