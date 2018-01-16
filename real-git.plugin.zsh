48e83fa17c04c4bc1bed2b1f0d964be3c6fa2946
Bastian Böhne <bastian.boehne@hitmeister.de>
2018-01-16 16:32:20 +0100

    removed debug echos

--- real-git.plugin.zsh
+++ real-git.plugin.zsh
@@ -4,14 +4,10 @@ function git_prompt_info() {
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
     ref=${ref#refs/heads/}
-    if [ "$(echo "$ref" | egrep -i "^dev-[0-9]+")" = "" ];
+    if [ "$(echo "$ref" | egrep -i "^dev-[0-9]+")" != "" ];
     then
-      echo "$ref"
-      echo "regex failed"
-      return 0
+	ref=$( echo "$ref" | egrep -io "dev-[0-9]+")
     fi
-    ref=$( echo "$ref" | egrep -io "dev-[0-9]+")
-    echo "ende angelangt"
     echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
