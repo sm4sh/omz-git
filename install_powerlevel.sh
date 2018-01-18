f8447db32858fbc333b99788fe7b5f35a82e31df
Bastian Böhne <bastian.boehne@hitmeister.de>
2018-01-18 15:58:09 +0100

    added safety to not install twice

--- install_powerlevel.sh
+++ install_powerlevel.sh
@@ -1,3 +1,13 @@
 cd ~/.oh-my-zsh/custom/themes/powerlevel9k/
+if [ -e real-mod.txt ]
+then
+  echo
+  echo "already installed"
+  echo
+  exit 1
+fi
 sed -i '1339 a \\n  if [ "$(echo "$vcs_prompt" | egrep -i "dev-[0-9]+")" != "" ];\n  then\n    vcs_prompt=$( echo "$vcs_prompt" | sed -E -e "s/(^.*?)(dev-[0-9]+)\\S*(\\s.*)/\\1\\2\\3/ig" )\n  fi' powerlevel9k.zsh-theme
-echo "successfully installed"
+echo "modified .zsh-theme file at line 1341-1344" > real-mod.txt
+echo
+echo "Restart your terminal to enable"
+echo
